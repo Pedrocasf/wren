272de3e8907119b28f79272ff9fd5d4e44e9ef53
pedrocasf <pedrostarling2000@gmail.com>
2024-09-27 18:47:35 -0300

    disable random native class

--- build.zig
+++ build.zig
@@ -34,26 +34,18 @@ pub fn build(b: *std.Build) !void {
 
     wren.link_eh_frame_hdr = true;
     wren.link_emit_relocs = false;
-    wren.addCSourceFiles(.{
-        .files = &.{
-            "src/vm/wren_compiler.c",
-            "src/vm/wren_core.c",
-            "src/vm/wren_debug.c",
-            "src/vm/wren_primitive.c",
-            "src/vm/wren_utils.c",
-            "src/vm/wren_value.c",
-            "src/vm/wren_vm.c",
-            "src/optional/wren_opt_meta.c",
-            "src/optional/wren_opt_random.c",
-        },
-        .flags = &.{
-            "-std=c99",
-            "-DWREN_OPT_META=1",
-            "-DWREN_OPT_RANDOM=1",
-            "-DWREN_NAN_TAGGING=0",
-            "-DWREN_COMPUTED_GOTO=1"
-        }});
-    wren.addIncludePath(.{.cwd_relative = "/usr/local/pspdev/psp/include/"});
+    wren.addCSourceFiles(.{ .files = &.{
+        "src/vm/wren_compiler.c",
+        "src/vm/wren_core.c",
+        "src/vm/wren_debug.c",
+        "src/vm/wren_primitive.c",
+        "src/vm/wren_utils.c",
+        "src/vm/wren_value.c",
+        "src/vm/wren_vm.c",
+        "src/optional/wren_opt_meta.c",
+        "src/optional/wren_opt_random.c",
+    }, .flags = &.{ "-std=c99", "-DWREN_OPT_META=1", "-DWREN_OPT_RANDOM=0", "-DWREN_NAN_TAGGING=0", "-DWREN_COMPUTED_GOTO=1" } });
+    wren.addIncludePath(.{ .cwd_relative = "/usr/local/pspdev/psp/include/" });
     wren.addIncludePath(b.path("src/include/"));
     wren.addIncludePath(b.path("src/vm/"));
     wren.addIncludePath(b.path("src/optional/"));
